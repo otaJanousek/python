eb5c3e4e7c3c1db4914e63131536dcf51eb27cc5
otaJanousek <144893436+otaJanousek@users.noreply.github.com>
2023-09-20 09:42:39 +0000

    2. hodina

--- cisla.py
+++ cisla.py
@@ -1,13 +1,21 @@
 import random
 
 c = 0
-x = input ("V jakém rozsahu chcete hádat? Vložte nejprve dolní hranici ")
-y = input("Vložte horní hranici ")
-a = (random.randrange(int(x), int(y)))
-while c == 0:
-    b = input ("Hádejte číslo mezi " + x + " a "+ y+" ")
-    if a == b:
-        print ("správně, hledané číslo bylo"+ a)
-        c = 1
-    else:
-        print("kreténe")
+typ= input("Chcete-li jrát proti počítači, napište 1. Pokud chcete hrát ve dvou, napište 2)")
+if typ = 1:
+    print("Zvolili jste hru proti počítači")
+    x = input ("V jakém rozsahu chcete hádat? Vložte nejprve dolní hranici ")
+    y = input("Vložte horní hranici ")
+    pokus = int(input("Vložte počet pokusů, který chcete mít:"))
+    a = (random.randrange(int(x), int(y)))
+    while c == 0:
+        b = int(input ("Hádejte číslo mezi " + x + " a "+ y+" "))
+        if a == b :
+            print ("správně, hledané číslo bylo", a)
+            c = 1
+        else:
+            print("Špatně zkuste znovu")
+else:
+    z = input("Vložte myšlené číslo: ")
+
+    
---
+++ ulohy.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "cherry"]
+y = 0
+for x in fruits:
+  print(x)
+  y = y+1
+  if y==2:
+    break
