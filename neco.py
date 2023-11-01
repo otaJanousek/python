d70ec0590bc038301aae32cc2a3b5eb8a76b4eec
otaJanousek <144893436+otaJanousek@users.noreply.github.com>
2023-11-01 11:08:53 +0100

    Update neco.py

--- neco.py
+++ neco.py
@@ -1,13 +1,17 @@
-a, b, c = 1, 2, -3
+a = 
+b = 
+c = 
 #a = int(input(Zadejte a:)
-b = int(input(Zadejte b:)
-c = int(input(Zadejte c:) #
-
-D = (pow(b, 2) - 4 * a * c)
-if D == 0:
-    print("1:", -b / (2 * a))
-elif D > 0:
-    print("1: ", (-b + (pow (D, 0,5))) / (2 * a),
-          "2: ", (-b - (pow(D, 0,5))) / (2 * a))
-else:
-    print("Rovnice nemá kořen")
+#b = int(input(Zadejte b:)
+#c = int(input(Zadejte c:)
+if a =! 0 and b=! 0 and c =! 0:
+    D = (pow(b, 2) - 4 * a * c)
+    if D == 0:
+        print("1:", -b / (2 * a))
+    elif D > 0:
+        print("1: ", (-b + (pow (D, 0,5))) / (2 * a),
+              "2: ", (-b - (pow(D, 0,5))) / (2 * a))
+    else:
+        print("Rovnice nemá kořen")
+elif a == 0:
+    
