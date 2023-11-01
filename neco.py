7fb9eada44a39e9ec155953a2455c820d024b42b
otaJanousek <144893436+otaJanousek@users.noreply.github.com>
2023-11-01 11:48:39 +0100

    Update neco.py

--- neco.py
+++ neco.py
@@ -1,17 +1,23 @@
-a = 
-b = 
-c = 
-#a = int(input(Zadejte a:)
-#b = int(input(Zadejte b:)
-#c = int(input(Zadejte c:)
-if a =! 0 and b=! 0 and c =! 0:
+
+a = int(input("Zadejte a:"))
+b = int(input("Zadejte b:"))
+c = int(input("Zadejte c:"))
+
+print("Řeším rovnici: ",a,"x2 *",b,"x *",c)
+if a != 0:
     D = (pow(b, 2) - 4 * a * c)
+    print(D)
     if D == 0:
-        print("1:", -b / (2 * a))
+        x = (-b / (2 * a))
+        print("1:", x )
+
     elif D > 0:
-        print("1: ", (-b + (pow (D, 0,5))) / (2 * a),
-              "2: ", (-b - (pow(D, 0,5))) / (2 * a))
+        x1 = (-b + (pow (D, 0,5))) / (2 * a)
+        x2 = (-b - (pow(D, 0,5))) / (2 * a)
+        print("1: ", x1,
+              "2: ", x2)
     else:
         print("Rovnice nemá kořen")
 elif a == 0:
-    
+    print("1: ", (-b/c))
+#nefunguje to :(
